38748708b85e1d1b41e69aad7ee3bea3c0d7669d
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2023-11-30 15:38:16 +0530

    implemented Comment Model

---
+++ model/comment/Comment.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema(
+	{
+		post: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Post",
+			required: [true, "Post is required"],
+		},
+		user: {
+			type: Object,
+			required: [true, "User is required"],
+		},
+		description: {
+			type: String,
+			required: [true, "Comment description required"],
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+module.exports = Comment;
