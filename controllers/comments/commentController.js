6a2421d733d56a83adbfcdc875ee53a30abf20a0
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2023-11-30 16:20:24 +0530

    implemented: post a new comment

--- controllers/comments/commentController.js
+++ controllers/comments/commentController.js
@@ -2,7 +2,21 @@ const expressAsyncHandler = require("express-async-handler");
 const Comment = require("../../model/comment/Comment");
 
 const createCommentController = expressAsyncHandler(async (req, res) => {
-	res.json("create a new Comment ");
+	//get the id of the post
+	const { postId, description } = req.body;
+	//get the user
+	const user = req.user;
+	try {
+		const comment = await Comment.create({
+			post: postId,
+			user: user,
+			description: description,
+		});
+
+		res.json(comment);
+	} catch (err) {
+		res.json(err);
+	}
 });
 
 module.exports = { createCommentController };
