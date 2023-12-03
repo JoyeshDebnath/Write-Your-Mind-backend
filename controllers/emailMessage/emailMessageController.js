7f42975f1fe9ad5a29364ac135cc55bf14979fa8
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2023-12-03 16:09:08 +0530

    added filters for Slangs in Email

--- controllers/emailMessage/emailMessageController.js
+++ controllers/emailMessage/emailMessageController.js
@@ -2,12 +2,22 @@ const EmailMessage = require("../../model/emailMessage/emailMessage");
 const expressAsyncHandler = require("express-async-handler");
 const validateMongodbId = require("../../utils/validateMongodbID");
 const sgMail = require("@sendgrid/mail");
-
+const SlangFilter = require("bad-words");
 //--------------------------------------------------------------------------
 //send Emails message
 //-------------------------------------------------------------------------
 const sendEmailMessageController = expressAsyncHandler(async (req, res) => {
 	const { to, subject, message } = req?.body;
+	//check for bad words in subject and email body
+	let emailMessage = subject + " " + message;
+	filter = new SlangFilter();
+	const isSlang = filter.isProfane(emailMessage);
+	if (isSlang) {
+		throw new Error(
+			"Email Contains Slangs ! Sending Email Failed ! Please Check your email"
+		);
+	}
+
 	try {
 		// set up message
 		const msg = {
@@ -22,6 +32,7 @@ const sendEmailMessageController = expressAsyncHandler(async (req, res) => {
 			from: "itsmejoyeshdebnath26@gmail.com",
 		};
 		//send message
+
 		await sgMail.send(msg);
 		//save the message to db
 		await EmailMessage.create({
